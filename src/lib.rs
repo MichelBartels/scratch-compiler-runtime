f45cd265a6cf0ccbf8b82cad52b726e55aef3370
Michel Bartels <login@michelbartels.com>
2024-09-19 20:58:54 +0100

    add support for most motion commands

--- src/lib.rs
+++ src/lib.rs
@@ -2,34 +2,36 @@ use std::ffi::{c_char, c_void, CStr};
 use std::fmt::Debug;
 use std::io::{self, BufRead, Write};
 use std::sync::RwLock;
+use std::thread::JoinHandle;
 
 mod ui;
+use ui::WrappedSprite;
 pub use ui::{create_window, new_scene, new_sprite, scene_add_sprite};
 
 #[no_mangle]
-pub extern "C" fn alloc_string(c_str: *const c_char) -> *mut c_void {
+pub extern "C" fn alloc_string(c_str: *const c_char) -> *mut String {
     let c_str = unsafe { CStr::from_ptr(c_str) };
     let str = c_str.to_str().unwrap().to_owned();
     let boxed_str = Box::new(str);
-    Box::into_raw(boxed_str) as *mut c_void
+    Box::into_raw(boxed_str)
 }
 
 #[no_mangle]
-pub extern "C" fn free_string(ptr: *mut c_void) {
+pub extern "C" fn free_string(ptr: *mut String) {
     unsafe {
-        let _ = Box::from_raw(ptr as *mut String);
+        let _ = Box::from_raw(ptr);
     }
 }
 
 #[no_mangle]
-pub extern "C" fn say(ptr: *const c_void) {
-    let s = unsafe { &*(ptr as *const String) };
+pub extern "C" fn say(ptr: *const String) {
+    let s = unsafe { &*ptr };
     println!("{}", s);
 }
 
 #[no_mangle]
-pub extern "C" fn ask(question: *const c_void) -> *mut c_void {
-    let question = unsafe { &*(question as *const String) };
+pub extern "C" fn ask(question: *const String) -> *mut c_void {
+    let question = unsafe { &*question };
     print!("{} ", question);
     io::stdout().flush().unwrap();
     let mut input = String::new();
@@ -39,74 +41,71 @@ pub extern "C" fn ask(question: *const c_void) -> *mut c_void {
     Box::into_raw(boxed_input) as *mut c_void
 }
 
-fn alloc_empty_vec<T>() -> *mut c_void {
+fn alloc_empty_vec<T>() -> *mut RwLock<Vec<T>> {
     let vec: Vec<T> = Vec::new();
     let boxed_vec = Box::new(RwLock::new(vec));
-    Box::into_raw(boxed_vec) as *mut c_void
+    Box::into_raw(boxed_vec)
 }
 
 #[no_mangle]
-pub extern "C" fn alloc_empty_string_vec() -> *mut c_void {
+pub extern "C" fn alloc_empty_string_vec() -> *mut RwLock<Vec<String>> {
     alloc_empty_vec::<String>()
 }
 
 #[no_mangle]
-pub extern "C" fn alloc_empty_f64_vec() -> *mut c_void {
+pub extern "C" fn alloc_empty_f64_vec() -> *mut RwLock<Vec<f64>> {
     alloc_empty_vec::<f64>()
 }
 
 #[no_mangle]
-pub extern "C" fn alloc_empty_bool_vec() -> *mut c_void {
+pub extern "C" fn alloc_empty_bool_vec() -> *mut RwLock<Vec<bool>> {
     alloc_empty_vec::<bool>()
 }
 
-fn clear_vec<T: std::fmt::Debug>(ptr: *mut c_void) {
-    let ptr = ptr.cast::<RwLock<Vec<T>>>();
+fn clear_vec<T: std::fmt::Debug>(ptr: *mut RwLock<Vec<T>>) {
     let rwlock = unsafe { ptr.as_ref().unwrap() };
     let mut vec = rwlock.write().unwrap();
     vec.clear();
 }
 
 #[no_mangle]
-pub extern "C" fn clear_string_vec(ptr: *mut c_void) {
+pub extern "C" fn clear_string_vec(ptr: *mut RwLock<Vec<String>>) {
     clear_vec::<String>(ptr);
 }
 
 #[no_mangle]
-pub extern "C" fn clear_f64_vec(ptr: *mut c_void) {
+pub extern "C" fn clear_f64_vec(ptr: *mut RwLock<Vec<f64>>) {
     clear_vec::<f64>(ptr);
 }
 
 #[no_mangle]
-pub extern "C" fn clear_bool_vec(ptr: *mut c_void) {
+pub extern "C" fn clear_bool_vec(ptr: *mut RwLock<Vec<bool>>) {
     clear_vec::<bool>(ptr);
 }
 
-fn push_to_vec<T: Debug>(ptr: *mut c_void, value: T) {
-    let ptr = ptr.cast::<RwLock<Vec<T>>>();
+fn push_to_vec<T: Debug>(ptr: *mut RwLock<Vec<T>>, value: T) {
     let rwlock = unsafe { ptr.as_ref().unwrap() };
     let mut vec = rwlock.write().unwrap();
     vec.push(value);
 }
 
 #[no_mangle]
-pub extern "C" fn push_to_string_vec(ptr: *mut c_void, value: *const c_void) {
-    let value = unsafe { &*(value as *const String) };
+pub extern "C" fn push_to_string_vec(ptr: *mut RwLock<Vec<String>>, value: *const String) {
+    let value = unsafe { &*value };
     push_to_vec(ptr, value.clone());
 }
 
 #[no_mangle]
-pub extern "C" fn push_to_f64_vec(ptr: *mut c_void, value: f64) {
+pub extern "C" fn push_to_f64_vec(ptr: *mut RwLock<Vec<f64>>, value: f64) {
     push_to_vec(ptr, value);
 }
 
 #[no_mangle]
-pub extern "C" fn push_to_bool_vec(ptr: *mut c_void, value: bool) {
+pub extern "C" fn push_to_bool_vec(ptr: *mut RwLock<Vec<bool>>, value: bool) {
     push_to_vec(ptr, value);
 }
 
-fn get_vec_element<T: Clone + Debug>(ptr: *mut c_void, index: f64, default: T) -> T {
-    let ptr = ptr.cast::<RwLock<Vec<T>>>();
+fn get_vec_element<T: Clone + Debug>(ptr: *const RwLock<Vec<T>>, index: f64, default: T) -> T {
     let rwlock = unsafe { ptr.as_ref().unwrap() };
     let vec = rwlock.read().unwrap();
     let index = index as usize - 1;
@@ -114,24 +113,26 @@ fn get_vec_element<T: Clone + Debug>(ptr: *mut c_void, index: f64, default: T) -
 }
 
 #[no_mangle]
-pub extern "C" fn get_string_vec_element(ptr: *mut c_void, index: f64) -> *mut c_void {
+pub extern "C" fn get_string_vec_element(
+    ptr: *const RwLock<Vec<String>>,
+    index: f64,
+) -> *mut String {
     let element = get_vec_element(ptr, index, "".to_owned());
     let boxed_element = Box::new(element);
-    Box::into_raw(boxed_element) as *mut c_void
+    Box::into_raw(boxed_element)
 }
 
 #[no_mangle]
-pub extern "C" fn get_f64_vec_element(ptr: *mut c_void, index: f64) -> f64 {
+pub extern "C" fn get_f64_vec_element(ptr: *const RwLock<Vec<f64>>, index: f64) -> f64 {
     get_vec_element(ptr, index, 0.0)
 }
 
 #[no_mangle]
-pub extern "C" fn get_bool_vec_element(ptr: *mut c_void, index: f64) -> bool {
+pub extern "C" fn get_bool_vec_element(ptr: *const RwLock<Vec<bool>>, index: f64) -> bool {
     get_vec_element(ptr, index, false)
 }
 
-fn index_of<T: PartialEq + Debug>(ptr: *mut c_void, value: T) -> f64 {
-    let ptr = ptr.cast::<RwLock<Vec<T>>>();
+fn index_of<T: PartialEq + Debug>(ptr: *const RwLock<Vec<T>>, value: T) -> f64 {
     let rwlock = unsafe { ptr.as_ref().unwrap() };
     let vec = rwlock.read().unwrap();
     vec.iter()
@@ -141,24 +142,23 @@ fn index_of<T: PartialEq + Debug>(ptr: *mut c_void, value: T) -> f64 {
 }
 
 #[no_mangle]
-pub extern "C" fn index_of_string(vec: *mut c_void, value: *const c_void) -> f64 {
-    let value = unsafe { &*(value as *const String) };
+pub extern "C" fn index_of_string(vec: *const RwLock<Vec<String>>, value: *const String) -> f64 {
+    let value = unsafe { &*value };
     let index = index_of(vec, value.clone());
     index
 }
 
 #[no_mangle]
-pub extern "C" fn index_of_f64(vec: *mut c_void, value: f64) -> f64 {
+pub extern "C" fn index_of_f64(vec: *const RwLock<Vec<f64>>, value: f64) -> f64 {
     index_of(vec, value)
 }
 
 #[no_mangle]
-pub extern "C" fn index_of_bool(vec: *mut c_void, value: bool) -> f64 {
+pub extern "C" fn index_of_bool(vec: *const RwLock<Vec<bool>>, value: bool) -> f64 {
     index_of(vec, value)
 }
 
-fn set_vec_element<T>(ptr: *mut c_void, index: f64, value: T) {
-    let ptr = ptr.cast::<RwLock<Vec<T>>>();
+fn set_vec_element<T>(ptr: *const RwLock<Vec<T>>, index: f64, value: T) {
     let rwlock = unsafe { ptr.as_ref().unwrap() };
     let mut vec = rwlock.write().unwrap();
     let index = index as usize - 1;
@@ -168,44 +168,47 @@ fn set_vec_element<T>(ptr: *mut c_void, index: f64, value: T) {
 }
 
 #[no_mangle]
-pub extern "C" fn set_string_vec_element(ptr: *mut c_void, index: f64, value: *const c_void) {
-    let value = unsafe { &*(value as *const String) };
+pub extern "C" fn set_string_vec_element(
+    ptr: *const RwLock<Vec<String>>,
+    index: f64,
+    value: *const String,
+) {
+    let value = unsafe { &*value };
     set_vec_element(ptr, index, value.clone());
 }
 
 #[no_mangle]
-pub extern "C" fn set_f64_vec_element(ptr: *mut c_void, index: f64, value: f64) {
+pub extern "C" fn set_f64_vec_element(ptr: *const RwLock<Vec<f64>>, index: f64, value: f64) {
     set_vec_element(ptr, index, value);
 }
 
 #[no_mangle]
-pub extern "C" fn set_bool_vec_element(ptr: *mut c_void, index: f64, value: bool) {
+pub extern "C" fn set_bool_vec_element(ptr: *const RwLock<Vec<bool>>, index: f64, value: bool) {
     set_vec_element(ptr, index, value);
 }
 
-fn len_of_vec<T: Debug>(ptr: *mut c_void) -> f64 {
-    let ptr = ptr.cast::<RwLock<Vec<T>>>();
+fn len_of_vec<T: Debug>(ptr: *const RwLock<Vec<T>>) -> f64 {
     let rwlock = unsafe { ptr.as_ref().unwrap() };
     let vec = rwlock.read().unwrap();
     vec.len() as f64
 }
 
 #[no_mangle]
-pub extern "C" fn len_of_string_vec(ptr: *mut c_void) -> f64 {
+pub extern "C" fn len_of_string_vec(ptr: *const RwLock<Vec<String>>) -> f64 {
     len_of_vec::<String>(ptr)
 }
 
 #[no_mangle]
-pub extern "C" fn len_of_f64_vec(ptr: *mut c_void) -> f64 {
+pub extern "C" fn len_of_f64_vec(ptr: *const RwLock<Vec<f64>>) -> f64 {
     len_of_vec::<f64>(ptr)
 }
 
 #[no_mangle]
-pub extern "C" fn len_of_bool_vec(ptr: *mut c_void) -> f64 {
+pub extern "C" fn len_of_bool_vec(ptr: *const RwLock<Vec<bool>>) -> f64 {
     len_of_vec::<bool>(ptr)
 }
 
-fn cast_vec_to_string<T: ToString>(vec: *mut c_void) -> *mut c_void {
+fn cast_vec_to_string<T: ToString>(vec: *const RwLock<Vec<T>>) -> *mut String {
     let vec = unsafe { &*(vec as *const Vec<T>) };
     let string = vec
         .iter()
@@ -213,48 +216,48 @@ fn cast_vec_to_string<T: ToString>(vec: *mut c_void) -> *mut c_void {
         .collect::<Vec<String>>()
         .join(", ");
     let boxed_str = Box::new(string);
-    Box::into_raw(boxed_str) as *mut c_void
+    Box::into_raw(boxed_str)
 }
 
 #[no_mangle]
-pub extern "C" fn cast_string_vec_to_string(vec: *mut c_void) -> *mut c_void {
+pub extern "C" fn cast_string_vec_to_string(vec: *const RwLock<Vec<String>>) -> *mut String {
     cast_vec_to_string::<String>(vec)
 }
 
 #[no_mangle]
-pub extern "C" fn cast_f64_vec_to_string(vec: *mut c_void) -> *mut c_void {
+pub extern "C" fn cast_f64_vec_to_string(vec: *const RwLock<Vec<f64>>) -> *mut String {
     cast_vec_to_string::<f64>(vec)
 }
 
-fn cast_to_string<T: ToString>(val: T) -> *mut c_void {
+fn cast_to_string<T: ToString>(val: T) -> *mut String {
     let string = val.to_string();
     let boxed_str = Box::new(string);
-    Box::into_raw(boxed_str) as *mut c_void
+    Box::into_raw(boxed_str)
 }
 
 #[no_mangle]
-pub extern "C" fn cast_f64_to_string(value: f64) -> *mut c_void {
+pub extern "C" fn cast_f64_to_string(value: f64) -> *mut String {
     cast_to_string::<f64>(value)
 }
 
 #[no_mangle]
-pub extern "C" fn cast_string_to_f64(value: *const c_void) -> f64 {
-    let value = unsafe { &*(value as *const String) };
+pub extern "C" fn cast_string_to_f64(value: *const String) -> f64 {
+    let value = unsafe { &*(value) };
     value.parse().unwrap_or(0.0)
 }
 
 #[no_mangle]
-pub extern "C" fn join(string1: *const c_void, string2: *const c_void) -> *mut c_void {
+pub extern "C" fn join(string1: *const String, string2: *const String) -> *mut String {
     let string1 = unsafe { &*(string1 as *const String) };
     let string2 = unsafe { &*(string2 as *const String) };
     let joined = format!("{}{}", string1, string2);
     let boxed_str = Box::new(joined);
-    Box::into_raw(boxed_str) as *mut c_void
+    Box::into_raw(boxed_str)
 }
 
 #[no_mangle]
-pub extern "C" fn letter_of(string: *const c_void, index: f64) -> *mut c_void {
-    let string = unsafe { &*(string as *const String) };
+pub extern "C" fn letter_of(string: *const String, index: f64) -> *mut String {
+    let string = unsafe { &*(string) };
     let index = index as usize - 1;
     let letter = string
         .chars()
@@ -262,27 +265,33 @@ pub extern "C" fn letter_of(string: *const c_void, index: f64) -> *mut c_void {
         .map(|c| c.to_string())
         .unwrap_or("".to_string());
     let boxed_str = Box::new(letter);
-    Box::into_raw(boxed_str) as *mut c_void
+    Box::into_raw(boxed_str)
 }
 
 #[no_mangle]
-pub extern "C" fn string_eq(string1: *const c_void, string2: *const c_void) -> bool {
-    let string1 = unsafe { &*(string1 as *const String) };
-    let string2 = unsafe { &*(string2 as *const String) };
+pub extern "C" fn string_eq(string1: *const String, string2: *const String) -> bool {
+    let string1 = unsafe { &*(string1) };
+    let string2 = unsafe { &*(string2) };
     string1 == string2
 }
 
 #[no_mangle]
-pub extern "C" fn spawn_thread(unsafe_fn: extern "C" fn()) -> *mut c_void {
+pub extern "C" fn spawn_thread(
+    unsafe_fn: extern "C" fn(*const WrappedSprite),
+    sprite: *const WrappedSprite,
+) -> *mut JoinHandle<()> {
+    let sprite = unsafe { &*sprite };
+    let sprite = sprite.clone();
+    let sprite = Box::new(sprite);
     let handle = std::thread::spawn(move || {
-        unsafe_fn();
+        unsafe_fn(Box::into_raw(sprite));
     });
     let boxed_handle = Box::new(handle);
-    Box::into_raw(boxed_handle) as *mut c_void
+    Box::into_raw(boxed_handle)
 }
 
 #[no_mangle]
-pub extern "C" fn join_thread(handle: *mut c_void) {
-    let handle = unsafe { Box::from_raw(handle as *mut std::thread::JoinHandle<()>) };
+pub extern "C" fn join_thread(handle: *mut JoinHandle<()>) {
+    let handle = unsafe { Box::from_raw(handle) };
     handle.join().unwrap();
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,10 @@
 use std::ffi::{c_char, CStr};
-use std::sync::RwLock;
+use std::sync::{Arc, RwLock};
 
+use macroquad::math::Vec2;
+use macroquad::texture::{draw_texture_ex, DrawTextureParams};
 use macroquad::{
-    color, prelude::ImageFormat, texture::{draw_texture, Texture2D}, window::{clear_background, next_frame}, Window
+    color, prelude::ImageFormat, texture::Texture2D, window::{clear_background, next_frame}, Window
 };
 
 fn svg_to_texture(svg_str: &str) -> Texture2D {
@@ -21,77 +23,183 @@ fn svg_to_texture(svg_str: &str) -> Texture2D {
     Texture2D::from_file_with_format(&png, Some(ImageFormat::Png))
 }
 
-#[derive(Clone, Debug)]
-pub struct Sprite {
-    texture: Option<Texture2D>,
-    svg: String,
-    x: i32,
-    y: i32,
+enum LazyTexture {
+    Loaded(Texture2D),
+    Unloaded(String),
 }
 
-impl Sprite {
-    fn new(texture: Option<Texture2D>, svg: String, x: i32, y: i32) -> Self {
-        Self { texture, svg, x, y }
-    }
-
-    fn get_texture(&mut self) -> Texture2D {
-        if self.texture.is_none() {
-            self.texture = Some(svg_to_texture(&self.svg));
+impl LazyTexture {
+    fn get_texture(&mut self) -> &Texture2D {
+        match self {
+            Self::Loaded(texture) => texture,
+            Self::Unloaded(svg) => {
+                let texture = svg_to_texture(svg);
+                *self = Self::Loaded(texture);
+                self.get_texture()
+            }
         }
-        self.texture.clone().unwrap()
+    }
+}
+
+pub struct Costume {
+    svg: LazyTexture,
+    rotation_center_x: f32,
+    rotation_center_y: f32,
+}
+
+impl Costume {
+    fn new(svg: String, rotation_center_x: i32, rotation_center_y: i32) -> Self {
+        Self { svg: LazyTexture::Unloaded(svg), rotation_center_x: rotation_center_x as f32, rotation_center_y: rotation_center_y as f32 }
     }
 
-    fn draw(&mut self) {
-        draw_texture(&self.get_texture(), self.x as f32, self.y as f32, color::WHITE);
+    fn draw(&mut self, x: f32, y: f32, rotation: f32) {
+        draw_texture_ex(self.svg.get_texture(), x, y, color::WHITE, DrawTextureParams {
+            rotation: (rotation - 90.) * std::f32::consts::PI / 180.0,
+            pivot: Some(Vec2 {
+                x: self.rotation_center_x + x,
+                y: self.rotation_center_y + y,
+
+            }),
+            ..Default::default()
+        })
     }
 }
 
 #[no_mangle]
-pub fn new_sprite(svg_str: *const c_char, x: i32, y: i32) -> *mut Sprite {
-    let svg = unsafe { CStr::from_ptr(svg_str).to_str().unwrap() };
-    let sprite = Sprite::new(None, svg.to_owned(), x, y);
-    Box::into_raw(Box::new(sprite))
+pub fn new_costume(svg_str: *const c_char, x: i32, y: i32) -> *const Costume {
+    let svg_str = unsafe { CStr::from_ptr(svg_str).to_str().unwrap().to_owned() };
+    let costume = Costume::new(svg_str, x, y);
+    Box::into_raw(Box::new(costume))
+}
+
+pub struct Sprite {
+    costumes: Vec<Costume>,
+    current_costume: usize,
+    x: f32,
+    y: f32,
+    direction: f32,
+}
+
+impl Sprite {
+    fn draw(&mut self) {
+        let costume = &mut self.costumes[self.current_costume];
+        let x = 240. - costume.rotation_center_x + self.x;
+        let y = 180. - costume.rotation_center_y - self.y;
+        costume.draw(x, y, self.direction);
+    }
+}
+
+pub type WrappedSprite = Arc<RwLock<Sprite>>;
+
+#[no_mangle]
+pub fn new_sprite(current_costume: i32, x: f32, y: f32, direction: f32) -> *const WrappedSprite {
+    let sprite = Sprite {
+        costumes: Vec::new(),
+        current_costume: current_costume as usize,
+        x,
+        y,
+        direction,
+    };
+    let arc = Arc::new(RwLock::new(sprite));
+    Box::into_raw(Box::new(arc))
+}
+
+#[no_mangle]
+pub fn sprite_add_costume(sprite: *const WrappedSprite, costume: *mut Costume) {
+    let sprite = unsafe { &*sprite };
+    let costume = unsafe { Box::from_raw(costume) };
+    sprite.write().unwrap().costumes.push(*costume)
+}
+
+#[no_mangle]
+pub fn sprite_set_x(sprite: *const WrappedSprite, x: f64) {
+    let sprite = unsafe { &*sprite };
+    sprite.write().unwrap().x = x as f32;
+}
+
+#[no_mangle]
+pub fn sprite_set_y(sprite: *const WrappedSprite, y: f64) {
+    let sprite = unsafe { &*sprite };
+    sprite.write().unwrap().y = y as f32;
+}
+
+#[no_mangle]
+pub fn sprite_change_x(sprite: *const WrappedSprite, dx: f64) {
+    let sprite = unsafe { &*sprite };
+    sprite.write().unwrap().x += dx as f32;
+}
+
+#[no_mangle]
+pub fn sprite_change_y(sprite: *const WrappedSprite, dy: f64) {
+    let sprite = unsafe { &*sprite };
+    sprite.write().unwrap().y += dy as f32;
+}
+
+#[no_mangle]
+pub fn sprite_get_x(sprite: *const WrappedSprite) -> f64 {
+    let sprite = unsafe { &*sprite };
+    sprite.read().unwrap().x as f64
+}
+
+#[no_mangle]
+pub fn sprite_get_y(sprite: *const WrappedSprite) -> f64 {
+    let sprite = unsafe { &*sprite };
+    sprite.read().unwrap().y as f64
+}
+
+#[no_mangle]
+pub fn sprite_get_direction(sprite: *const WrappedSprite) -> f64 {
+    let sprite = unsafe { &*sprite };
+    sprite.read().unwrap().direction as f64
+}
+
+#[no_mangle]
+pub fn sprite_turn_right(sprite: *const WrappedSprite, degrees: f64) {
+    let sprite = unsafe { &*sprite };
+    sprite.write().unwrap().direction += degrees as f32;
+}
+
+#[no_mangle]
+pub fn sprite_turn_left(sprite: *const WrappedSprite, degrees: f64) {
+    let sprite = unsafe { &*sprite };
+    sprite.write().unwrap().direction -= degrees as f32;
+}
+
+#[no_mangle]
+pub fn sprite_move_steps(sprite: *const WrappedSprite, steps: f64) {
+    let sprite = unsafe { &*sprite };
+    let mut sprite = sprite.write().unwrap();
+    let direction = sprite.direction.to_radians();
+    sprite.x -= steps as f32 * direction.cos();
+    sprite.y -= steps as f32 * direction.sin();
 }
 
 pub struct Scene {
-    sprites: RwLock<Vec<Sprite>>,
+    sprites: Vec<WrappedSprite>,
 }
 
 impl Scene {
-    fn new() -> Self {
-        Self { sprites: RwLock::new(Vec::new()) }
-    }
-
-    fn add_sprite(&self, sprite: Sprite) {
-        self.sprites.write().unwrap().push(sprite);
-    }
-
     fn draw(&self) {
-        for sprite in self.sprites.write().unwrap().iter_mut() {
-            sprite.draw();
+        for sprite in self.sprites.iter() {
+            sprite.write().unwrap().draw();
         }
     }
 }
 
 #[no_mangle]
 pub fn new_scene() -> *const Scene {
-    println!("Creating new scene");
-    let ptr = Box::into_raw(Box::new(Scene::new()));
-    println!("Scene: {:?}", ptr);
-    ptr
+    Box::into_raw(Box::new(Scene { sprites: Vec::new()}))
 }
 
 #[no_mangle]
-pub fn scene_add_sprite(scene: *const Scene, sprite: *const Sprite) {
-    println!("Adding sprite to scene");
-    let scene = unsafe { &*scene };
+pub fn scene_add_sprite(scene: *mut Scene, sprite: *const WrappedSprite) {
+    let scene = unsafe { &mut *scene };
     let sprite = unsafe { &*sprite };
-    scene.add_sprite(sprite.clone());
+    scene.sprites.push(sprite.clone());
 }
 
 #[no_mangle]
 pub fn create_window(scene: *const Scene) {
-    println!("Creating window");
     let scene = unsafe { &*scene };
     Window::from_config(macroquad::conf::Conf {
         miniquad_conf: miniquad::conf::Conf {
